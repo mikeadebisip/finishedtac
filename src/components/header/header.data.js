9cd823a1b9c2a743b2aee0dc1bc85f6c78ee822a
Michael E Adebisi <adebisienads@gmail.com>
2022-03-17 17:57:34 +0100

    ready for deplo

---
+++ src/components/header/header.data.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    path: "/",
+    label: "TAC Scholarship"
+  },
+  {
+    path: "https://theafternoonchurch.org",
+    label: "The Afternoon Church",
+  },
+  {
+    path: "https://pastakachi.com",
+    label: "Past Akachi Nwoke",
+  },
+  {
+    path: `https://support.${process.env.HOSTED_DOMAIN_NAME}`,
+    label: "Sponsor Us",
+  },
+];
